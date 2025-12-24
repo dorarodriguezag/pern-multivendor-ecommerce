47318ab7ddc8d162aee5d9f701d47b28642a50c7
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2025-12-23 22:46:05 -0600

    Revert "use serverless-safe Prisma singleton with Neon adapter"
    This reverts commit eeff33ec3eaa173d4f534ec1f2cd0f2f179e7e71.

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,9 +2,6 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
 
-
-export const runtime = "nodejs";
-
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
