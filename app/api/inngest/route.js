b538a14bd10d3eade36a13d40bdb83566c50fe26
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2025-12-24 19:32:50 -0600

    console logs for debugging

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,12 @@
+export const runtime = "nodejs";
+
+console.log("🔥 [ROUTE] /api/inngest route file loaded");
+
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
 
-
-export const runtime = "nodejs";
+console.log("🔥 [ROUTE] imports resolved");
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -14,3 +17,5 @@ export const { GET, POST, PUT } = serve({
     syncUserDeletion,    /* your functions will be passed here later! */
   ],
 });
+
+console.log("🔥 [ROUTE] serve() executed");
