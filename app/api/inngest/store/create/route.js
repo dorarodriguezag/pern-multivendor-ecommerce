7afb14a22f6fa60ff10485c31e22b9e7d065e16b
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2025-12-30 19:29:27 -0600

    finish create store api

--- app/api/inngest/store/create/route.js
+++ app/api/inngest/store/create/route.js
@@ -50,14 +50,62 @@ export async function POST(request) {
         const optimizedImage = imagekit.url({
             path: response.filePath,
             transformation: [
-                {quallity: 'auto'},
+                {quality: 'auto'},
                 {format: 'webp'},
+                {width: '512'},
             ]
         });
 
+        const newStore = await prisma.store.create({
+            data: {
+                userId,
+                name,
+                description,
+                username: username.toLowerCase(),
+                email,
+                contact,
+                address,
+                logo: optimizedImage,
+            }
+        })
+
+        //link store to user
+        await prisma.user.update({
+            where: { id: userId },
+            data: {store: {connect: {id: newStore.id}}}
+        })
+
+        return NextResponse.json({message: "applied, waiting for approval"})
 
     } catch (error) {
-    
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message}, { status: 400 })
     }
 
+}
+
+
+// check user have already registered a store if yes then send status of store
+
+export async function GET(request) {
+    try {
+
+        const { userId } = getAuth(request)
+
+        // check if user have already a store
+        const store = await prisma.store.findFirst({
+            where: { userId: userId }
+        })
+
+        // if store is already registered then send status of store
+        if(store){
+            return NextResponse.json({status: store.status})
+        }
+
+        return NextResponse.json({status: 'not registered'})
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message}, { status: 400 })
+    }
 }
