13bcb7fbbf8d1a2972cd725b10b525e7630d053f
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2026-01-02 21:16:49 -0600

    complete product api and add stock toggle api

--- app/api/store/product/route.js
+++ app/api/store/product/route.js
@@ -62,3 +62,27 @@ export async function POST(request) {
   }
 
 }
+
+// Get all products for a seller
+export async function GET(request) {
+    try {
+        
+    const { userId } = getAuth(request);
+        const storeId = await authSeller(userId);
+
+        if (!storeId) {
+        return NextResponse.json({error: 'not authorized'}, {status: 401});
+        }
+
+        const products = await prisma.product.findMany({
+            where: {
+                storeId
+            }
+        })
+
+        return NextResponse.json({products})
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message}, { status: 400 })     
+    }    
+}
