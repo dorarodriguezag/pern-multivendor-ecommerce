f08ca2f78484b8da704d966f9adb9a580dfccb92
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2026-01-04 14:34:04 -0600

    create admin/is-admin api route

--- app/api/admin/is-admin/route.js
+++ app/api/admin/is-admin/route.js
@@ -1,25 +1,19 @@
-import { getPrisma } from "@/lib/prisma";
-import authSeller from "@/middlewares/authSeller";
+import authAdmin from "@/middlewares/authAdmin";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
-// Auth Seller
+// Auth Admin
 export async function GET(request) {
     try {
-        const prisma = getPrisma();
-
         const { userId } = getAuth(request)
-        const isSeller = await authSeller(userId)
+        const isAdmin = await authAdmin(userId)
 
-        if(!isSeller){
+        if(!isAdmin){
             return NextResponse.json({error: "not authorized"}, {status: 401})
         }
-        const storeInfo = await prisma.store.findFirst({
-            where: { userId }
-        })
 
-        return NextResponse.json({isSeller, storeInfo})
+        return NextResponse.json({isAdmin})
         
     } catch (error) {
         console.error(error);
