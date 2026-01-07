3a8d9085ac5f4019c4ea5fdf526430f39386da6a
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2026-01-06 20:46:19 -0600

    add userBotton component to AdminNavbar

--- components/admin/AdminNavbar.jsx
+++ components/admin/AdminNavbar.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useUser } from "@clerk/nextjs";
+import { useUser, userBotton } from "@clerk/nextjs";
 import Link from "next/link"
 
 const AdminNavbar = () => {
@@ -16,6 +16,7 @@ const AdminNavbar = () => {
             </Link>
             <div className="flex items-center gap-3">
                 <p>Hi, {user?.firstName}</p>
+                <userBotton />
             </div>
         </div>
     )
