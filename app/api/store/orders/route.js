b8972b8df2b9c1bdb506218ec0dc43f89c305b95
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2026-01-24 23:21:23 -0600

    implement get endpoint to retrieve orders

--- app/api/store/orders/route.js
+++ app/api/store/orders/route.js
@@ -28,3 +28,28 @@ export async function POST(request) {
         return NextResponse.json({error: error.code || error.message}, { status: 400 })           
     }
 }
+
+
+// Get all orders for a seller
+export async function GET(request) {
+    try {
+        const prisma = getPrisma();
+        const { userId } = getAuth(request);
+        const storeId = await authSeller(userId);
+
+         if(!storeId){
+            return NextResponse.json({error: "not authorized"}, {status: 401})
+        }
+
+        const orders = await prisma.order.findMany({
+            where: { storeId},
+            include: { user: true, address: true, orderItems: { include: {product: true}}},
+            orderBy: { createdAt: 'desc' }
+        })
+
+        return NextResponse.json({ orders }) 
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message}, { status: 400 })           
+    }
+}
