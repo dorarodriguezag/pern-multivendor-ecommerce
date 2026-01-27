64e7b9b7fa83b9e2034e0dae70ae015ab4c7e556
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2026-01-26 18:16:40 -0600

    implement user cart route (post and get endpoints)

---
+++ app/api/cart/route.js
@@ -0,0 +1,46 @@
+import { getPrisma } from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// Update user cart 
+export async function POST(request) {
+    try {
+        const prisma = getPrisma();
+        const { userId } = getAuth(request)
+        const { cart } = await request.json()
+
+        // Save the cart to user object
+        await prisma.user.update({
+            where: { id: userId },
+            data: { cart }
+        })
+            return NextResponse.json({ message: "Cart updated" })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message}, { status: 400 })     
+    }
+}
+
+import { getPrisma } from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+
+// Get user cart 
+export async function GET(request) {
+    try {
+        const prisma = getPrisma();
+        const { userId } = getAuth(request)
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+        })
+            return NextResponse.json({ cart: user.cart })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message}, { status: 400 })     
+    }
+}
--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,5 +1,4 @@
 import { getPrisma } from "@/lib/prisma";
-import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
