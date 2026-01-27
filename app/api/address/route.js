ee87c9ffc73e91d9015ca11113cc93af03639d1a
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2026-01-26 19:04:50 -0600

    implement address management route (post and get endpoints)

---
+++ app/api/address/route.js
@@ -0,0 +1,44 @@
+import { getPrisma } from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// add new address
+export async function POST(request) {
+    try {
+        const prisma = getPrisma();
+        const { userId } = getAuth(request)
+        const { address } = await request.json()
+
+        address.userId = userId
+
+        // Save the cart to user object
+        const newAddress = await prisma.address.create({
+            data: address,
+            data: { cart }
+        })
+        return NextResponse.json({ newAddress,  message: "Address added succesfully" })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message}, { status: 400 })     
+    }
+}
+
+
+
+// Get all addresses for a user
+export async function GET(request) {
+    try {
+        const prisma = getPrisma();
+        const { userId } = getAuth(request)
+
+        const addresses = await prisma.address.findMany({
+            whre: { userId},
+        })
+        return NextResponse.json({ addresses })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({error: error.code || error.message}, { status: 400 })     
+    }
+}
