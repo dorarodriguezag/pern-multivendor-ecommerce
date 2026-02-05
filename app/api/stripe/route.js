0e4d1d99a4385099d0b1d0b3854e12c951f7cec2
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2026-02-05 12:51:08 -0600

    implement Stripe payment integration and webhook handling

---
+++ app/api/stripe/route.js
@@ -0,0 +1,79 @@
+import { getPrisma } from "@/lib/prisma";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(request) {
+    try {
+        const prisma = getPrisma();
+        const body = await request.text()
+        const sig = request.get('stripe-signature')
+
+        const event = stripe.webhooks.constructEvent(body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+
+        const handlePaymentIntent = async (paymentIntentId, isPaid) => {
+            const session = await stripe.checkout.sessions.list({
+                payment_intent: paymentIntentId,
+            })
+
+            const { orderIds, userId, appId } = session.data[0].metadata;
+
+            if(appId !== 'gocart'){
+                return NextResponse.json({ received: true, message: 'Invalid app id' })
+            }
+
+            const orderIdsArray = orderIds.split(',');
+
+            if(isPaid){
+                //mark orders as paid
+                await Promise.all(orderIdsArray.map(async (orderId) => {
+                    await prisma.order.update({
+                        where: { id: orderId },
+                        data: { isPaid: true }
+                    })
+                }))
+                // delete cart from user
+                await prisma.user.update({
+                    where: { id: userId },
+                    data: { cart: {} }
+                })
+            }else{
+                // delete unpaid orders
+                await Promise.all(orderIdsArray.map(async (orderId) => {
+                    await prisma.order.delete({
+                        where: { id: orderId }
+                    })
+                }))
+
+            }
+        }
+
+        switch (event.type) {
+            case 'payment_intent.succeeded': {
+                await handlePaymentIntent(event.data.object.id, true);
+                break;
+            }
+
+            case 'payment_intent.canceled': {
+                await handlePaymentIntent(event.data.object.id, true);
+                break;
+            }
+
+            default:
+                console.log('Unhandled event type:', event.type);
+                break
+        }
+
+        return NextResponse.json({ received: true })
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error.message}, { status: 400 });
+        
+    }
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
