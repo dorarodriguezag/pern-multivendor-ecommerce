048ceb5c9ea6ba5fa382355d094926bb82318ffb
Dora Rodriguez <dora.rodriguez.prg@gmail.com>
2026-02-04 22:46:27 -0600

    implement fetchUserRatings logic and integrate into PublicLayout

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -9,6 +9,7 @@ import { useUser } from "@clerk/nextjs";
 import { useAuth } from "@clerk/clerk-react";
 import { fetchCart, uploadCart } from "@/lib/features/cart/cartSlice";
 import { fetchAddress } from "@/lib/features/address/addressSlice";
+import { fetchUserRatings } from "@/lib/features/rating/ratingSlice";
 
 export default function PublicLayout({ children }) {
 
@@ -32,6 +33,7 @@ export default function PublicLayout({ children }) {
         if (user) {
             dispatch(uploadCart({getToken}) );
             dispatch(fetchAddress({getToken}) );
+            dispatch(fetchUserRatings({getToken}));
         }
     }, [cartItems]);
 
